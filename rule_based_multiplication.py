bdce572c6a7223a80012cf3d5b430b3648ac57d7
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2019-11-30 10:51:45 +0530

    Create rule_based_multiplication.py

---
+++ rule_based_multiplication.py
@@ -0,0 +1,18 @@
+#PF-Assgn-15
+
+def find_product(num1,num2,num3):
+    product=0
+    #write your logic here
+    if num1==7:
+        product=num2*num3
+    elif num2==7:
+        product=num3
+    elif num3==7:
+        product=-1
+    else:
+        product=num1*num2*num3
+    return product
+
+#Provide different values for num1, num2, num3 and test your program
+product=find_product(7,6,2)
+print(product)
