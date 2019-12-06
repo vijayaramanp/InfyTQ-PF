1ae9a2ef4b342cfa4832589ceade58e50b0b8b49
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2019-12-06 11:32:51 +0530

    Create day2_pf_ex-9.py

---
+++ day2_pf_ex-9.py
@@ -0,0 +1,16 @@
+//PF-Exer-9
+
+noOfFlightsTakeOff=100
+noOfFlightsLanded=110
+seatingCapacityTakeOff=150
+seatingCapacityLanded=185
+totalCookies=0
+
+//Write your code here
+totalCookies=(noOfFlightsTakeOff*seatingCapacityTakeOff+noOfFlightsLanded*(seatingCapacityLanded/2))*2
+
+
+//Do not modify the below print statement for verification to work
+console.log(totalCookies)
+
+
