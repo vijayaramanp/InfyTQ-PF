8cd1aa87a2535f997ede256c577e220143995fc7
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-01-24 12:21:49 +0530

    Create day3_Ex_18_adjacent_count.py

---
+++ day3_Ex_18_adjacent_count.py
@@ -0,0 +1,12 @@
+#PF-Exer-18
+
+def get_count(num_list):
+    count=0
+    for i in range(0,len(num_list)-1):
+        if num_list[i]==num_list[i+1]:
+            count+=1 
+    return count
+
+#provide different values in list and test your program
+num_list=[1,1,5,100,-20,-20,6,0,0]
+print(get_count(num_list))
