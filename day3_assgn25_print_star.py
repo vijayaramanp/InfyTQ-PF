7d77363f72e861f5573feacf49329f0d0ac7e5c5
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-02-24 15:42:22 +0530

    Create day3_assgn25_print_star.py

---
+++ day3_assgn25_print_star.py
@@ -0,0 +1,11 @@
+#PF-Tryout
+
+counter1=0
+while(counter1 < 5):
+    counter2=5
+    star=""
+    while(counter2>counter1):
+        star=star+ "*"
+        counter2-=1
+    print(star)
+    counter1+=1
