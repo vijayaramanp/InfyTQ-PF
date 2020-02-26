002f654b83a1577b3e8e7630151c7d6d7b584501
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-02-26 14:33:34 +0530

    Create day4_ex_23_dict.py

---
+++ day4_ex_23_dict.py
@@ -0,0 +1,19 @@
+#PF-Exer-23
+def translate(bilingual_dict,english_words_list):
+    
+    swedish_words_list=[]
+    for word in english_words_list:
+        swedish_words_list.append(bilingual_dict[word])
+
+
+    return swedish_words_list
+
+
+bilingual_dict= {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"}
+english_words_list=["merry","christmas"]
+
+print("The bilingual dict is:",bilingual_dict)
+print("The english words are:",english_words_list)
+
+swedish_words_list=translate(bilingual_dict, english_words_list)
+print("The equivalent swedish words are:",swedish_words_list)
