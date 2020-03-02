d53f24ec2439b485bd810a484b06778604e98654
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-02 12:32:47 +0530

    Create day4_ex_25_find_sum.js

---
+++ day4_ex_25_find_sum.js
@@ -0,0 +1,15 @@
+//JS-Exer-25
+
+//Start writing your code here
+find_sum(n){
+    var i,sum=0;
+    for(i=1;i<=n;i++){
+        sum=sum+i
+    }
+    return sum;
+}
+
+
+var n=6,res=0;
+res=find_sum(n);
+console.log("Result"+res);
