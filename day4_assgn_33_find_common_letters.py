06619864324b8ea7d21e2021bacef0f15866a820
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-02 16:09:56 +0530

    Update day4_assgn_33_find_common_letters.py

--- day4_assgn_33_find_common_letters.py
+++ day4_assgn_33_find_common_letters.py
@@ -19,3 +19,25 @@ msg1="I like Python"
 msg2="Java is a very popular language"
 common_characters=find_common_characters(msg1,msg2)
 print(common_characters)
+
+#Solution 2: 50% test cases passed
+'''#PF-Assgn-33
+
+def find_common_characters(msg1,msg2):
+    res=""
+    temp=list(sorted(set(msg1)&set(msg2)))
+    for char in temp:
+        if char!=' ':
+            res=res+char
+    if res==' ':
+        return -1
+    else:
+        res=res
+        return res
+
+#Provide different values for msg1,msg2 and test your program
+msg1="Python"
+msg2="python"
+common_characters=find_common_characters(msg1,msg2)
+print(common_characters)
+'''
