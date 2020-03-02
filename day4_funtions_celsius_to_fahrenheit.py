7fe47fabf8bd81f7e2ef56ebf9426e7efe1b9b57
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-02 11:08:10 +0530

    Create day4_funtions_celsius_to_fahrenheit.py

---
+++ day4_funtions_celsius_to_fahrenheit.py
@@ -0,0 +1,15 @@
+#goal of this tryout is to create a function from scratch and invoke it for the given problem
+def celsius_to_fahrenheit(celsius):
+    fahrenheit=(celsius*9/5)+32
+    return fahrenheit
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius=(fahrenheit-32)*(5/9)
+    return celsius
+c=1.0
+f=celsius_to_fahrenheit(c)
+print("%.2f Celsius is %0.2f Fahrenheit " %(c,f))
+
+f=1.0
+c=fahrenheit_to_celsius(f)
+print('%.2f Fahrenheit is %0.2f Celsius ' %(f,c))
