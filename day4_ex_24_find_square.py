e7adc12919039af15d35b345ef251fde23131c6f
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-02 12:23:27 +0530

    Create day4_ex_24_find_square.py

---
+++ day4_ex_24_find_square.py
@@ -0,0 +1,13 @@
+//PF-Exer-24
+//This verification is based on string match.
+
+package main
+
+import ("fmt")
+func find_square(n int)(int){
+    return n*n
+}
+func main() {
+    var n int=3
+    fmt.Println(find_square(n))
+}
