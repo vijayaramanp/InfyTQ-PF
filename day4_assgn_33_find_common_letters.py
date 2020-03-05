10787fde6e6989bc68ed26d2feb0992731008ab5
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-05 18:51:48 +0530

    Update day4_assgn_33_find_common_letters.py

--- day4_assgn_33_find_common_letters.py
+++ day4_assgn_33_find_common_letters.py
@@ -1,5 +1,24 @@
 #PF-Assgn-33
+#Solution 1
+def find_common_characters(msg1,msg2):
+    res=''
+    for char in msg1:
+        if char in msg2 and char!=' ' and char not in res:
+                res=res+char
+    if res=='':
+        return -1
+    else:
+        
+        return res
+#Provide different values for msg1,msg2 and test your program
+msg1="moto"
+msg2="moto"
+common_characters=find_common_characters(msg1,msg2)
+print(common_characters)
 
+
+'''
+#Solution 2
 def find_common_characters(msg1,msg2):
     res=''
     for char in msg1:
@@ -16,8 +35,9 @@ msg1="moto"
 msg2="moto"
 common_characters=find_common_characters(msg1,msg2)
 print(common_characters)
-
 '''
+'''
+#Solution 3
 def find_common_characters(msg1,msg2):
     temp=""
     for a in msg1:
@@ -40,6 +60,7 @@ print(common_characters)
 '''
 
 '''
+#Solution 4
 # This solution passess
 5 out of 6 test cases passed.
 
@@ -68,8 +89,9 @@ common_characters=find_common_characters(msg1,msg2)
 print(common_characters)
 
 '''
-#Solution 2: 50% of the test cases passes
+#Solution 5: 50% of the test cases passes
 '''
+
 #PF-Assgn-33
 
 def find_common_characters(msg1,msg2):
@@ -90,7 +112,7 @@ msg2="python"
 common_characters=find_common_characters(msg1,msg2)
 print(common_characters)
 '''
-#solution 3
+#solution 6
 '''
 #PF-Assgn-33
 def find_common_characters(msg1,msg2):
