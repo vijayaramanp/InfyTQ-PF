21dee016b0a414516fe7f4e39319add39d8be0d1
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-06 14:37:20 +0530

    Create day5_ex_29_merge_and_sort.py

---
+++ day5_ex_29_merge_and_sort.py
@@ -0,0 +1,13 @@
+#PF-Exer-29
+def merge_lists(list1,list2):
+    new_merge_list=list1+list2
+    return new_merge_list
+    
+def sort_list(merged_list):
+    return sorted(merged_list)
+
+#Provide different values for list1 and list2 and test your program
+merged_list=merge_lists(list1=[1,2,3,4,1] ,list2=[2,3,4,5,4,6])
+print(merged_list)
+sorted_merged_list=sort_list(merged_list)
+print(sorted_merged_list)
