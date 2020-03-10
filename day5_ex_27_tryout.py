2bfc0f3e5e7b60eb15d02824c2aa881f82b502e8
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-10 14:03:56 +0530

    Update day5_ex_27_tryout.py

--- day5_ex_27_tryout.py
+++ day5_ex_27_tryout.py
@@ -1,6 +1,4 @@
-#PF-Tryout
-
-def withdraw_transcation(amount,account_number,value):
+def withdraw_transcation(amount,value):
     balance=balance_list[value]
     new_balance=balance-amount
     if(new_balance >= 500):
@@ -10,16 +8,16 @@ def withdraw_transcation(amount,account_number,value):
     else:
         print("Insufficient Balance")
     
-def deposit_transcation(amount,account_number,value):
+def deposit_transcation(amount,value):
         balance=balance_list[value]
         new_balance=balance+amount
         balance_list[value]=new_balance
         print("Transaction completed successfully")
         print("Balance Amount :", new_balance)
         
-def balance_enquiry(account_number,value):
+def balance_enquiry(value):
     balance=balance_list[value]
-    print(balance)
+    print("Balance Amount :", balance)
     
 def validate_account_number_and_transcation_type():
     flag=None 
@@ -31,18 +29,18 @@ def validate_account_number_and_transcation_type():
             break
     if flag==True:
         if(transaction_type=="Withdraw"):
-            withdraw_transcation(amount,account_number,value)
+            withdraw_transcation(amount,value)
         elif(transaction_type=="Deposit"):
-            deposit_transcation(amount,account_number,value)
+            deposit_transcation(amount,value)
         elif(transaction_type=="Balance Enquiry"):
-            balance_enquiry(account_number,value)
+            balance_enquiry(value)
         else:
             print("Invalid Transaction Type")
             
 account_list=[1001,1002,1003,1004,1005]
 balance_list=[2500,10000,7000,1500,500]
-amount=1000
+amount=2000
 account_number=1003
-transaction_type="Balance Enquiry"
+transaction_type="Withdraw"
 
 validate_account_number_and_transcation_type()
