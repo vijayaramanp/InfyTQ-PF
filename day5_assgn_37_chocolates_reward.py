5e0357369df8838b083bdd97d36264b61550f308
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-11 14:45:36 +0530

    Create day5_assgn_37_chocolates_reward.py

---
+++ day5_assgn_37_chocolates_reward.py
@@ -0,0 +1,24 @@
+#PF-Assgn-37
+
+#Global variables
+child_id=(101, 201, 301, 401, 501)
+chocolates_received=[10, 5, 3, 2, 4]
+
+def calculate_total_chocolates():
+    return sum(chocolates_received)
+
+def reward_child(child_id_rewarded,extra_chocolates):
+    if extra_chocolates>0:
+        if child_id_rewarded in child_id:
+            child_id_index=child_id.index(child_id_rewarded)
+            chocolates_received[child_id_index]+=1
+            print(chocolates_received)
+        else:
+            print("Child id is invalid")
+    else:
+        print("Extra chocolates is less than 1")    
+
+print(calculate_total_chocolates())
+#Test your code by passing different values for child_id_rewarded,extra_chocolates
+reward_child(201,2)
+
