108d5bba7899887db101a5e102515394fee260e8
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-11 14:58:19 +0530

    Create day5_assgn_38_check_double_of_number.py

---
+++ day5_assgn_38_check_double_of_number.py
@@ -0,0 +1,18 @@
+#PF-Assgn-38
+def check_double(number):
+    double_of_number=2*number
+    str_num=str(number)
+    str_dbl_num=str(double_of_number)
+    flag=0
+    if len(str_num)==len(str_dbl_num):
+        for i in range(0,len(str_num)):
+            if str_num[i] in str_dbl_num:
+                continue
+            else:
+                return False 
+    else:
+        return False
+        
+    return True
+#Provide different values for number and test your program
+print(check_double(9))
