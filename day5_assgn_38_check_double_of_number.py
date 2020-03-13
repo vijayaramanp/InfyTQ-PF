b779e316657cb5e92f607c2c144d2b1d91f8a98c
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-13 09:27:28 +0530

    Update day5_assgn_38_check_double_of_number.py

--- day5_assgn_38_check_double_of_number.py
+++ day5_assgn_38_check_double_of_number.py
@@ -3,7 +3,6 @@ def check_double(number):
     double_of_number=2*number
     str_num=str(number)
     str_dbl_num=str(double_of_number)
-    flag=0
     if len(str_num)==len(str_dbl_num):
         for i in range(0,len(str_num)):
             if str_num[i] in str_dbl_num:
