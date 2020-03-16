a58315bbac69d298b2adc905d91a50849f54e85c
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-16 12:40:50 +0530

    Update day6_exception_ex_30.py

--- day6_exception_ex_30.py
+++ day6_exception_ex_30.py
@@ -22,3 +22,60 @@ try:
     print("Average marks:", find_average(m_list))
 except:
     print("Some Error")
+
+#Statement 3 case 3
+
+#PF-Exer-30
+
+def find_average(mark_list):
+	total=0
+	try:
+	    for i in range(0, len(mark_list)):
+	        total+=mark_list[i]
+	    marks_avg=total/len(mark_list)
+	    return marks_avg
+	except TypeError:
+	    print("Wrong data type")
+	except NameError:
+	    print("Name Error")
+
+try:
+    mark1="A"
+    mark1=int("A")
+    m_list=[mark1,2,3,4]
+    print("Average marks:", find_average(m_list))
+except NameError:
+    print("Name Error from main")
+except TypeError:
+    print("Type error from main")
+except ValueError:
+    print("value error from main")
+
+# statement 3 case 3
+#PF-Exer-30
+
+def find_average(mark_list):
+	total=0
+	try:
+	    for i in range(0, len(mark_list)+1):
+	        total+=mark_list[i]
+	    marks_avg=total/len(mark_list)
+	    return marks_avg
+	except TypeError:
+	    print("Wrong data type")
+	except NameError:
+	    print("Name Error")
+	except ZeroDivisionError:
+	    print("ZeroDivisionError")
+	except IndexError:
+	    print("index our of range")
+
+try:
+    m_list=[]
+    print("Average marks:", find_average(m_list))
+except NameError:
+    print("Name Error from main")
+except TypeError:
+    print("Type error from main")
+except ValueError:
+    print("value error from main")
