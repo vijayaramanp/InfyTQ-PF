be431e2336ffbd48c3e3598de72a4b86142c3348
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-20 14:16:06 +0530

    Create day6_ex_32_human_tower_max_people.py

---
+++ day6_ex_32_human_tower_max_people.py
@@ -0,0 +1,24 @@
+#PF-Exer-32
+
+def human_pyramid(no_of_people):
+    if (no_of_people==1):
+        return 1*50
+    else:
+        return no_of_people*50+human_pyramid(no_of_people-2)
+
+def find_maximum_people(max_weight):
+    no_of_people=1
+    prev_val=0
+    while True:
+        weight = human_pyramid(no_of_people)    
+        
+        if weight <= max_weight:
+            no_of_people += 2
+        else:
+            break
+        
+    return no_of_people-2
+
+#Provide different values for max_weight and test your program
+max_people=find_maximum_people(1000)
+print(max_people)
