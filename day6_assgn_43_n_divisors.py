962d1c9c2f8796dbc238cda99ad66eab04e86372
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-24 16:54:11 +0530

    Create day6_assgn_43_n_divisors.py

---
+++ day6_assgn_43_n_divisors.py
@@ -0,0 +1,19 @@
+Incomplete code
+
+#PF-Assgn-43
+def find_divisors(num):
+    divisors=[]
+    for i in range(2, num+1):
+        if num%i==0:
+            divisors.append(i)
+    return len(divisors)
+
+def find_smallest_number(num):
+    for i in range(2, 1000):
+        if find_divisors(i) == num:
+            return i
+    
+num=16
+print("The number of divisors :",num)
+result=find_smallest_number(num)
+print("The smallest number having",num," divisors:",result)
