633858d763520a3ef743b258f251d2f9911dd23a
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-25 17:25:43 +0530

    Create day7_ex_34_coffe_combinations.py

---
+++ day7_ex_34_coffe_combinations.py
@@ -0,0 +1,14 @@
+#PF-Exer-34
+import math
+def find_number_of_combination(number_of_flavours):
+    total_combination=0
+    if number_of_flavours == 0:
+        total_combination = 1 
+    else:
+        total_combination = math.factorial(number_of_flavours) + 1
+    
+    return total_combination
+
+#Provide different values for number_of_flavours and test your program
+number_of_combination=find_number_of_combination(4)
+print(number_of_combination)
