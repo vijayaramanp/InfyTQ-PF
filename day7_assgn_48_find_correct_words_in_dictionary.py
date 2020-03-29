fa6ef14fedb54fb23aa561ff251ac1eeb3f751bb
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-29 13:37:02 +0530

    Create day7_assgn_48_find_correct_words_in_dictionary.py

---
+++ day7_assgn_48_find_correct_words_in_dictionary.py
@@ -0,0 +1,27 @@
+#PF-Assgn-48
+
+def find_correct(word_dict):
+    correct_list=[0]*3
+    correct=0
+    almost_correct=0
+    wrong=0
+    for key,value in word_dict.items():
+        if len(key) == len(value):
+            difference=0
+            for i in range(len(key)):
+                if key[i]==value[i]:
+                    continue
+                else:
+                    difference+=1
+            if difference == 0:
+                correct_list[0]+=1
+            elif difference < 3:
+                correct_list[1]+=1 
+            else:
+                correct_list[2]+=1
+        else:
+           correct_list[2]+=1 
+    return correct_list
+
+word_dict={"THEIR": "THEIR","BUSINESS":"BISINESS","WINDOWS":"WINDMILL","WERE":"WEAR","SAMPLE":"SAMPLE"}
+print(find_correct(word_dict))
