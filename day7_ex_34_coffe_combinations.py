4fb467a65b230198100e1d4228e38cd1a8bda6e6
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-29 13:19:41 +0530

    Update day7_ex_34_coffe_combinations.py

--- day7_ex_34_coffe_combinations.py
+++ day7_ex_34_coffe_combinations.py
@@ -5,8 +5,8 @@ import math
 def find_number_of_combination(number_of_flavours):
     total_combination=0
     n=number_of_flavours
+    x=math.factorial(n)
     for r in range(number_of_flavours+1):
-        x=math.factorial(n)
         y=math.factorial(r)
         z=math.factorial(n-r)
         c=x/(y*z)
@@ -18,20 +18,3 @@ def find_number_of_combination(number_of_flavours):
 #Provide different values for number_of_flavours and test your program
 number_of_combination=find_number_of_combination(6)
 print(number_of_combination)
-
-'''
-#PF-Exer-34
-import math
-def find_number_of_combination(number_of_flavours):
-    total_combination=0
-    if number_of_flavours == 0:
-        total_combination = 1 
-    else:
-        total_combination = math.factorial(number_of_flavours) + 1
-    
-    return total_combination
-
-#Provide different values for number_of_flavours and test your program
-number_of_combination=find_number_of_combination(4)
-print(number_of_combination)
-'''
