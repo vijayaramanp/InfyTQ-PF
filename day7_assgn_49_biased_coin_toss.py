2be1ade95e5e1055a7773c36b0a7ce5476c945b1
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-03-29 17:58:33 +0530

    Update day7_assgn_49_biased_coin_toss.py

--- day7_assgn_49_biased_coin_toss.py
+++ day7_assgn_49_biased_coin_toss.py
@@ -2,9 +2,24 @@
 #Start writing your code here
 import random
 probability=0.7
-for i in range(1,7):
+for i in range(1,1001):
     if random.random()<=probability:
         print("Head")
     else:
         print("Tail")
     
+    
+'''Testing
+
+#PF-Tryout
+#Start writing your code here
+import random
+probability=0.7
+lis=[0]*2
+for i in range(1,1001):
+    if random.random()<=probability:
+        lis[0]+=1
+    else:
+        lis[1]+=1
+print(lis)
+'''
