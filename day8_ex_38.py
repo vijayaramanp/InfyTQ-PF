922f1ede4b134374e29edd3dbc70b3dfaf4bb0ac
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-04-03 16:17:54 +0530

    Create day8_ex_38.py

---
+++ day8_ex_38.py
@@ -0,0 +1,15 @@
+#PF-Exer-38
+#This verification is based on string match.
+import math
+num1=36
+num2=7
+num3=18
+
+calc = lambda num1,num2:num1%num2+num1-num2
+print(calc(num1,num2))
+
+square_root = lambda num3:math.sqrt(num3)
+print("{:.1f}".format(square_root(num3)))
+
+square_root2= lambda num3: num3**0.5
+print("{:.1f}".format(square_root2(num3)))
