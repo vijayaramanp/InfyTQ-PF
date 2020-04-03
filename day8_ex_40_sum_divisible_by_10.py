8f48114a383ec22d7297615d2327761574ed34e8
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2020-04-03 16:26:32 +0530

    Update day8_ex_40_sum_divisible_by_10.py

--- day8_ex_40_sum_divisible_by_10.py
+++ day8_ex_40_sum_divisible_by_10.py
@@ -10,27 +10,3 @@ if(div(num1,num2)%10)==0:
     print("Divisible by 10")
 else:
     print("Not Divisible by 10")
-
-
-def sum_all(function, data):
-    result_sum=0;
-    for w in data:
-        if(function(w)):
-            result_sum = result_sum+ w
-    return result_sum
-
-Q=[1,3,4,5,6,7,8,9,10,15,20]
-
-p = lambda x:x
-
-q = lambda x : x%2 == 0
-
-r = lambda x : x%3 == 0
-
-s = lambda x : x%5 == 0 
-
-
-print(sum_all(p,Q))
-print(sum_all(q,Q))
-print(sum_all(r,Q))
-print(sum_all(s,Q))
